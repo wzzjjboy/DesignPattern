f0e65571c012cbad517e916aca5fd8ea2c674ff1
Alan <wzzjjboy@126.com>
2016-11-01 18:05:19 +0800

    单例模式

---
+++ singleton/SingleObject.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @description:
+ * @author: wzzjjboy@126.com
+ * DateTime: 2016/11/1 17:59
+ */
+
+namespace designPattern\singleton;
+
+
+class SingleObject
+{
+    public static $instance = null;
+
+    public static function instance()
+    {
+        if(is_null(self::$instance)){
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+
+    public function doSomeThing()
+    {
+        println("hello php!");
+    }
+}
---
+++ singleton/index.php
@@ -0,0 +1,14 @@
+<?php
+namespace designPattern\singleton;
+
+/**
+ * @description:
+ * @author: wzzjjboy@126.com
+ * DateTime: 2016/11/1 17:59
+ */
+
+include "../init.php";
+
+SingleObject::instance()->doSomeThing();
+SingleObject::instance()->doSomeThing();
+
